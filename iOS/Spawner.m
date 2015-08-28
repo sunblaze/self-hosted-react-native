73669a532cf6c7bde280bb615f35b94e81a9cf31
James Reid-Smith <james.reidsmith@shopify.com>
2015-08-28 16:06:47 -0400

    Major tweaks to make developing a bit easier on device

--- iOS/Spawner.m
+++ iOS/Spawner.m
@@ -115,4 +115,37 @@ RCT_EXPORT_METHOD(curentSource:(RCTResponseSenderBlock)callback) {
   callback(@[[NSNull null], source]);
 }
 
+RCT_EXPORT_METHOD(versions:(RCTResponseSenderBlock)callback) {
+  NSInteger version = 1;
+  NSMutableArray *versions = [[NSMutableArray alloc] init];
+
+  while([[NSFileManager defaultManager] fileExistsAtPath:[[self class] codeFilePath:version]]){
+    NSNumber* versionWrapped = [NSNumber numberWithInt:version];
+    [versions addObject:versionWrapped];
+    version++;
+  }
+
+  callback(@[versions]);
+}
+
+RCT_EXPORT_METHOD(source:(NSInteger)version callback:(RCTResponseSenderBlock)callback) {
+  NSString *source = [NSString stringWithContentsOfFile:[[self class] codeFilePath:version] encoding:NSUTF8StringEncoding error:nil];
+  callback(@[[NSNull null], source]);
+}
+
+RCT_EXPORT_METHOD(loadedVersion:(RCTResponseSenderBlock)callback) {
+  NSNumber *version = [NSNumber numberWithInt:[[self class] loadedVersion]];
+  callback(@[version]);
+}
+
+RCT_EXPORT_METHOD(currentVersion:(RCTResponseSenderBlock)callback) {
+  NSInteger version = [[self class] loadedVersion];
+  NSInteger nextVersion = version + 1;
+  if([[NSFileManager defaultManager] fileExistsAtPath:[[self class] codeFilePath:nextVersion]]){
+    callback(@[[NSNumber numberWithInt:nextVersion]]);
+  }else{
+    callback(@[[NSNumber numberWithInt:version]]);
+  }
+}
+
 @end
--- index.ios.js
+++ index.ios.js
@@ -8,14 +8,19 @@ var Text = React.Text;
 var View = React.View;
 var TextInput = React.TextInput;
 var TouchableHighlight = React.TouchableHighlight;
+var AsyncStorage = React.AsyncStorage;
+var ScrollView = React.ScrollView;
+var Picker = React.PickerIOS;
+var PickerItem = Picker.Item;
 
 var Spawner = require('react-native/Libraries/react-native/react-native.js').NativeModules.Spawner;
-
 
 var MyFirstApp = React.createClass({
   displayName: 'MyFirstApp',
   getInitialState: function() {
-    return {text: ""};
+    return {text: "", versions: [1,2,3],
+            pickerVersion: 1,
+            loadedVersion: 0};
   },
   buttonClicked: function() {
     Spawner.spawn(this.state.text);
@@ -24,25 +29,63 @@ var MyFirstApp = React.createClass({
     this.setState({text: text});
   },
   componentWillMount: function() {
+    Spawner.currentVersion(function(version){
+      this.setState({pickerVersion: version});
+    }.bind(this));
+    Spawner.loadedVersion(function(version){
+      this.setState({loadedVersion: version});
+    }.bind(this));
+    Spawner.versions(function(versions){
+      this.setState({versions: versions});
+    }.bind(this));
+
     Spawner.curentSource(this.curentSource);
   },
   curentSource: function(error, source) {
     this.setState({text: source});
   },
+  versionChanged: function(version){
+    this.setState({pickerVersion: version});
+    Spawner.source(version,function(error,code){
+      this.setState({text: code});
+    }.bind(this));
+  },
+  doneClicked: function() {
+    this.refs.code.blur();
+  },
   render:      function(){
+    var versions = this.state.versions.map(function(version){
+      return React.createElement(PickerItem, {
+        key: version,
+        value: version,
+        label: "Version "+version});
+    });
     return (
       React.createElement(View, {style:styles.container},
-        React.createElement(TextInput, {style:styles.welcome,
+        React.createElement(ScrollView, {style:styles.scroll,
+ horizontal:true,
+ keyboardShouldPersistTaps: true}, React.createElement(TextInput, {style:styles.codeInput,
                                         value: this.state.text,
                                         multiline: true,
                                         textAlign: 'end',
                                         autoCorrect: false,
                                         autoCapitalize: 'none',
-                                        onChangeText: this.codeChanged}),
-        React.createElement(TouchableHighlight, {style:styles.button,
+                                        onChangeText: this.codeChanged,
+                                        ref: 'code'})),
+        React.createElement(View, {style: styles.tray},
+          [React.createElement(Text, {style: styles.vText}, 'v.'+this.state.loadedVersion),
+          React.createElement(TouchableHighlight, {style:styles.button,
+                                                 onPress: this.doneClicked,
+                                                 underlayColor: '#aabbee'},
+            React.createElement(Text, {style: styles.buttonText}, 'Hide Keyboard')),
+          React.createElement(TouchableHighlight, {style:styles.button,
                                                  onPress: this.buttonClicked,
                                                  underlayColor: '#aabbee'},
-          React.createElement(Text, {style: styles.buttonText}, 'Reload')),
+            React.createElement(Text, {style: styles.buttonText}, 'Run!'))
+          ]),
+        React.createElement(Picker,{style: styles.picker,
+                                    selectedValue: this.state.pickerVersion,
+                                    onValueChange: this.versionChanged},versions),
         React.createElement(View, {style:styles.fill})));
   }
 });
@@ -54,12 +97,16 @@ var styles = StyleSheet.create({
     alignItems:'center',
     backgroundColor:'#F5FCFF'
   },
-  welcome: {
+  scroll: {
     flex:1,
+    width: 375
+  },
+  codeInput: {
+    width: 1000,
+    height: 3000,
     fontFamily: 'Courier New',
     fontSize:16,
     textAlign:'left',
-    height: 100,
     borderColor: 'gray',
     borderWidth: 1,
     margin:10,
@@ -72,6 +119,8 @@ var styles = StyleSheet.create({
     marginBottom:5
   },
   button: {
+    margin: 20,
+    marginTop: 0,
     marginBottom: 7
   },
   buttonText: {
@@ -82,7 +131,20 @@ var styles = StyleSheet.create({
     color: '#007aff'
   },
   fill: {
-    height: 220
+    height: 0
+  },
+  picker: {
+    height: 210,
+    width: 375
+  },
+  tray: {
+    flexDirection: 'row'
+  },
+  vText: {
+    fontFamily: '.HelveticaNeueInterface-MediumP4',
+    fontSize: 17,
+    width: 80,
+    textAlign: 'left',
   }
 });
 
